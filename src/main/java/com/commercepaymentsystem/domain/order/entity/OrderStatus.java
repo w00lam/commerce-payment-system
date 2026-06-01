cbf895b746b9adf1d69fdc1a9d181054c8fa7dfe
jy030506@naver.com <jy030506@naver.com>
2026-06-01 14:52:35 +0900

    feat: 주문 상태 관리를 위한 OrderStatus 파일 추가

---
+++ src/main/java/com/commercepaymentsystem/domain/order/entity/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.commercepaymentsystem.domain.order.entity;
+
+public enum OrderStatus {
+
+	CREATED,    // 주문 생성됨, 결제 전
+	CONFIRMED,  // 결제 완료로 주문 확정
+	CANCELED,   // 결제 실패/사용자 취소로 주문 취소
+	REFUNDED    // 전체 환불됨
+}
