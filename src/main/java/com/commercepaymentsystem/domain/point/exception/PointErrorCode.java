e1c45ae99c5c7ffdf3c778afe5bdfaae10b4e1d3
JeonYongWoon <oioipo22@gmail.com>
2026-06-02 15:25:14 +0900

    feat: 포인트 복구 API 구현

--- src/main/java/com/commercepaymentsystem/domain/point/exception/PointErrorCode.java
+++ src/main/java/com/commercepaymentsystem/domain/point/exception/PointErrorCode.java
@@ -17,16 +17,16 @@ public enum PointErrorCode implements ErrorCode {
 		"포인트 금액은 0보다 커야 합니다."
 	),
 
-	INSUFFICIENT_POINT(
-		HttpStatus.BAD_REQUEST,
-		"POINT_002",
-		"보유 포인트가 부족합니다."
-	),
-
 	PAYMENT_ID_REQUIRED(
 		HttpStatus.BAD_REQUEST,
-		"POINT_003",
+		"POINT_002",
 		"결제 식별자는 필수입니다."
+	),
+
+	SOURCE_HISTORY_NOT_FOUND(
+		HttpStatus.NOT_FOUND,
+		"POINT_003",
+		"원본 차감 내역을 찾을 수 없어 복구가 불가능합니다."
 	);
 
 	private final HttpStatus status;
