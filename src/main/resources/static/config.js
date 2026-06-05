6ceac7c0d6f6d6f0a5e3cfbe0cc43db23b4069d0
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-06-05 16:44:44 +0900

    feat: Commerce Payment API Tester 프론트엔드 구축 및 config.js 제거

--- src/main/resources/static/config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-    storeId: "store-e18740d2-1f6b-4e5c-8a6a-492733c922cd",           // PortOne 콘솔에서 발급
-    channelKey: "channel-key-de40b17c-7a19-47c8-a788-41fc62288076",           // KG이니시스 인증결제 채널
-    subscriptionChannelKey: "channel-key-여기에-토스-구독-키", // 토스페이먼츠 빌링키 채널 (구독 전용)
-};
